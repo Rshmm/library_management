a53a6e12dfe5d2a011c55f22e66624f050620293
arsham <arshsm.hajeb@gamil.com>
2025-03-10 17:40:42 +0330

    ...

---
+++ with_oop/intanses_of_library.py
@@ -0,0 +1,10 @@
+class Book:
+    def __init__(self,code,name):
+        self.code =  code
+        self.name = name
+
+class Person:
+    def __init__(self,code,name,family):
+        self.code = code
+        self.name = name
+        self.family = family
